3d2d9e7c3aa5ceec042bbf7a530991538b215380
maniaghili <aligholimani@gmail.com>
2024-04-13 20:57:45 +0330

    s

---
+++ class/class.js
@@ -0,0 +1,28 @@
+
+
+class person{
+     
+      constructor(name,age,job){
+     this.namee = name
+     this.agee = age
+     this.jobe = job
+    
+      } 
+
+      run = ()=>{
+        console.log(`${this.namee}کارشو شروع کرد`)
+      }
+
+}
+
+
+class kerson extends person{
+    constructor(kir,name,age,job){
+        super(name,age,job)
+        this.kir = kir
+        console.log(kir,name,age,job)
+    }
+
+
+}
+let kkk = new kerson('kir','jaa',77,'web')
